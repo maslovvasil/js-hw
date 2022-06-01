681ee775f74617d79af62299c458a91c3c980cd1
Vasyl <maslovvasil@gmail.com>
2022-06-01 12:29:45 +0300

    task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import throttle from "lodash.throttle";
 
+const currentTimeKey = 'videoplayer-current-time';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Vimeo.Player(iframe);
+
+function timeUpdate(event) {
+    localStorage.setItem(currentTimeKey, event.seconds);
+}
+
+player.setCurrentTime(
+    localStorage.getItem(currentTimeKey) ? localStorage.getItem(currentTimeKey) : 0,
+);
+player.on('timeupdate', throttle(timeUpdate, 1000));
